d99b9b2bbdfba88cfe2c76815070d057fb1b120b
Kamol Treewatchararat <Kamol.Treewatchararat@telescope.tv>
2014-07-28 11:55:38 -0700

    approved-by-Bo-version

--- app/scripts/views/home.js
+++ app/scripts/views/home.js
@@ -8,6 +8,34 @@ Toptrending.Views = Toptrending.Views || {};
     , events: {
         'click .route-error': 'routeError'
       , 'click .route-loading': 'routeLoading'
+      , 'click .item': 'hitme'
+    }
+
+    , hitme: function(e) {
+      this.swap(4);
+    }
+
+    , swap: function (id) {
+      var self_id = id;
+      var next_id = self_id + 1;
+      var $self = $(".a" + self_id);
+      var $next = $(".a" + next_id);
+      var $selfhtml = $self.html();
+      var $nexthtml = $next.html();
+      var duration = 200;
+      $self.addClass('movedown shadow').delay(duration/2).queue(function() {
+        $(this).removeClass('shadow').dequeue();
+      }).delay(duration/2).queue(function(){
+        $(this).html($nexthtml);
+        $(this).removeClass('movedown').dequeue();
+      });
+      $next.addClass('moveup shadow').delay(duration/2).queue(function() {
+        $(this).removeClass('shadow').dequeue();
+      }).delay(duration/2).queue(function() {
+        $(this).html($selfhtml);
+        $(this).removeClass('moveup').dequeue();
+      });
+
     }
     /**
      *
